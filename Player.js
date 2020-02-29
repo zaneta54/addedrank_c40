9824c2856e85fd16fd8c84640d8b50c08f3350ed
zaneta54 <60514057+zaneta54@users.noreply.github.com>
2020-02-29 20:09:13 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,45 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+   static updaterank(rank){
+    database.ref('/').update({
+    playerrank : rank
+    });
+   }
+   getrank(){
+     database.ref('playerrank').on("value",(data)=>{
+      this.rank = data.val();
+     })
+   }
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
